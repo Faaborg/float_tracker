5d1735fbedd60abcb8a58a1d8dc096ff0dbf9610
Faaborg <faaborg@g.harvard.edu>
2019-07-29 17:31:32 -0400

    Cosmetic changes

--- float_tracker.py
+++ float_tracker.py
@@ -43,19 +43,26 @@ def ColorAverage(data):
     return np.array([red,green,blue]).astype(int)
 
 def ColorSTD(data):
+<<<<<<< HEAD
+    colorstd = np.array([np.std(data[:,:,0]),np.std(data[:,:,1]),np.std(data[:,:,2])])
+=======
     redSTD = np.std(data[:,:,0])   
     greenSTD = np.std(data[:,:,1])
     blueSTD = np.std(data[:,:,2])
     
     colorstd = np.array([redSTD,greenSTD,blueSTD])
     
+>>>>>>> ebdb5ca1bb9dc52bd28e783b78445ce3a40cde95
     return colorstd
 
 #filter using the full (cropped) image and the supercrop of a specific float
 def Filter(image,supercrop):
     floatcoloraverage = ColorAverage(supercrop)
     floatcolorSTD = ColorSTD(supercrop)
+<<<<<<< HEAD
+=======
     floatcolorSTD[floatcolorSTD<1]=1 #to keep everything from exploding when the average and std of a channel are 0, which actually happened
+>>>>>>> ebdb5ca1bb9dc52bd28e783b78445ce3a40cde95
     
     zeros=np.zeros([len(image[:]),len(image[0,:])])
     filtered=zeros
@@ -65,7 +72,11 @@ def Filter(image,supercrop):
     bluesignal = abs(image[:,:,2]-floatcoloraverage[2]) < floatcolorSTD[2]*bluecutoff
     fullmatch = np.logical_and(redsignal,greensignal,bluesignal)
     
+<<<<<<< HEAD
+    filtered[fullmatch]=100
+=======
     filtered[fullmatch]=1
+>>>>>>> ebdb5ca1bb9dc52bd28e783b78445ce3a40cde95
     return filtered
 
 
@@ -73,7 +84,11 @@ def Filter(image,supercrop):
 """The following will need to be tweaked manually for each analysis. 
 It is crucial that for each video, you do the supercrop around each float to establish the float colors"""
 #read data
+<<<<<<< HEAD
+img = io.imread('/home/miles/Desktop/Python/float_tracker/data/6794/scene00001.png')
+=======
 img = io.imread('/home/miles/Desktop/Python/data/6794/scene00001.png')
+>>>>>>> ebdb5ca1bb9dc52bd28e783b78445ce3a40cde95
 
 #crops to remove uneccesary information
 crop = img[300:400,500:900,:]
@@ -85,9 +100,15 @@ supercrop_bluefloat = crop[45:60,290:300,:]
 
 #set filter thresholds, each value is the number of 
 #standard deviations from the mean value of each channel of the supercrop of each float
+<<<<<<< HEAD
+redcutoff = 1
+greencutoff = 1
+bluecutoff = 1
+=======
 redcutoff = 3
 greencutoff = 3
 bluecutoff = 3
+>>>>>>> ebdb5ca1bb9dc52bd28e783b78445ce3a40cde95
 
 
 """""""business end"""""""""
@@ -99,9 +120,14 @@ yellowfloat = Filter(crop,supercrop_yellowfloat)
 bluefloat = Filter(crop,supercrop_bluefloat)
 
 """""""printouts:"""""""
+<<<<<<< HEAD
+print(COM(redfloat))
+plt.imshow(redfloat)
+=======
 print(COM(bluefloat))
 plt.imshow(bluefloat)
 plt.plot()
+>>>>>>> ebdb5ca1bb9dc52bd28e783b78445ce3a40cde95
 #print("red", np.average(supercrop_redfloat[:,:,0]))
 #print("green", np.average(supercrop_redfloat[:,:,1]))
 #print("blue", np.average(supercrop_redfloat[:,:,2]))
